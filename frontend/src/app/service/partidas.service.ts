7c7358e7297def0e3efabeeafd6cde7b4da66952
juliadsilva <juliadanielesilva@outlook.com>
2021-06-01 21:08:36 -0300

    Deleted workglows

--- frontend/src/app/service/partidas.service.ts
+++ frontend/src/app/service/partidas.service.ts
@@ -1,9 +1,5 @@
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-@Injectable({
-  providedIn: 'root'
-})
 export class PartidasService {
 
   constructor(private http:HttpClient) { }
